6888bb61166e24ab2cca5223ca0f83ada14fe762
LukasMandrup <AU617683@post.au.dk>
2023-12-05 16:36:06 +0100

    fix library

--- Package/index.ts
+++ Package/index.ts
@@ -1,3 +1,6 @@
-export * from './src/index';
+import  { AnalyticsWebClient, AnalyticsWebClientBuilder } from './src/index';
+
+export { AnalyticsWebClient, AnalyticsWebClientBuilder };
+	
 export * from './src/Models/index';
 export * from './src/Managers/index';
